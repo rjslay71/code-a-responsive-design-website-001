d2502f1177a5121cf994cf22197ff8b4a26d9583
rjslay71 <rs03148@ravens.racc.edu>
2018-02-11 22:24:38 -0500

    drop down menu added assignemnt 006

--- psd-to-wp-master/images/menu.js/menu.js
+++ psd-to-wp-master/images/menu.js/menu.js
@@ -1,10 +1,8 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
-
-<body>
-</body>
-</html>
+$(document).ready(function(){
+	
+	$(".burger-nav").on("click", function(){
+		
+		$("header nav ul").toggleClass("open");
+	});
+	
+});
